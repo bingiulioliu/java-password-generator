0133e78a0c558fa3b8ee859b03684443a8ff4be6
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-07-22 14:36:05 +0200

    FEAT aggiunto generatore pw

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,5 +1,15 @@
 package org.lessons.java.security;
-
 public class PasswordGenerator {
-    
+    public static void main(String[] args) {
+        String nome = "Pinco";
+        String cognome = "Pallo";
+        String colorePrefe = "magenta";
+        int giornoNascita = 12;
+        int meseNascita = 5;
+        int annoNascita = 1994;
+        
+        int sommaNascita = giornoNascita + meseNascita + annoNascita;
+
+        System.out.println("La password è: " + nome + "-" + cognome + "-" + colorePrefe + "-" + sommaNascita);
+    }
 }
